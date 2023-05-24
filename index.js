89055d1cc5b4493b6fbe8d192e217706d7d6506d
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2023-05-24 17:46:17 +0300

    [add] render main page

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Bri');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 5000;
+
+//Setting up Handlebars
+const exphbs = require('express-handlebars');
+const handlebars = exphbs.create({extname:'.hbs'});
+app.engine('.hbs',handlebars.engine);
+app.set('view engine', '.hbs')
+
+//Setting up server whith express
+app.get('/', (req, res) => {
+    res.set('views', (path.join(__dirname,'views')))
+    res.render('layouts/main');
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port: ${PORT}`);
+})
+
+
